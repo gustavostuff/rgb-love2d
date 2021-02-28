34d077008e485c358592f7907916a920cb69c0a1
tavuntu <ing.glaragomez@gmail.com>
2021-02-27 19:34:23 -0700

    added refresher

--- rgb-love-11.3-setup.sh
+++ rgb-love-11.3-setup.sh
@@ -1,14 +1,23 @@
+green='\033[0;32m'
+default='\033[0m'
+
 echo 'Installing dependencies...'
 sudo apt-get install build-essential autotools-dev automake libtool \
 pkg-config libfreetype6-dev libluajit-5.1-dev libphysfs-dev libsdl2-dev \
 libopenal-dev libogg-dev libvorbis-dev libmodplug-dev libmpg123-dev \
 libtheora-dev
 
-# get specific version
+# get love (precompiled)
 wget https://raw.githubusercontent.com/tavuntu/rgb-love2d/master/love-11.3.tar.gz
 
-# build and compile
+# setup love and love games (demos)
 tar -xf love-11.3.tar.gz
 rm love-11.3.tar.gz
 sudo ln -s /home/pi/love-11.3/src/love /usr/bin/
-echo 'Love2D is now available by just typing the command love'
+chmod +x __RefreshRoms
+cp __RefreshRoms ~/RetroPie/roms/ports/Love2D/
+./__RefreshRoms
+
+echo "*******************************************************"
+echo "${green}Setup completed!${default}"
+echo "${green}Go to RGB-Pi OS options screen and do a game search, you'll find something in Ports${default}"
